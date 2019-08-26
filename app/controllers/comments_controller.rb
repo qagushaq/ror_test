23b829be3343b64d563506b1304358a8c5e677fb
qagushaq <qwerty24164444@gmail.com>
2019-08-26 18:58:53 +0300

    upload

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,12 @@
+class CommentsController < ApplicationController
+  http_basic_authenticate_with name: "admin", password: "1",
+  only: [:destroy]
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+    redirect_to post_path(@post)
+  end
+  private def comment_params
+    params.require(:comment).permit(:username, :body)
+  end
+end
---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,6 @@
+class PagesController < ApplicationController
+  def about
+      @heading ='Страничка про нас!'
+      @text ='Litle bit text of corse my dear'
+  end
+end
